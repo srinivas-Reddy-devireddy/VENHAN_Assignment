5469b8f99603411c590006c77e2ea3196adfb5b4
srinivas-Reddy-devireddy <121668154+srinivas-Reddy-devireddy@users.noreply.github.com>
2024-12-27 06:11:53 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ function App() {
             </ErrorBoundary>
           </div>
           <div className="col-md-9 col-12">
+          <h1 style={{ textAlign: 'center', margin: 'auto', display: 'block' ,padding: '10px 0',fontSize:"19px"}}>
+      Diagram Flow
+          </h1>
             <DiagramFlow />
           </div>
         </div>
